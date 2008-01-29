bc0b20a462d1adeb7026645dc014c78d44fbd9fa
rdewolff <rdewolff@90076dac-8a41-0410-b28f-0f618caa8d27>
2008-01-29 20:02:02 +0000

    ok equipe <-> joueur

--- trunk/app/models/equipe.rb
+++ trunk/app/models/equipe.rb
@@ -1,6 +1,6 @@
 class Equipe < ActiveRecord::Base
   belongs_to :entraineur
   belongs_to :nation, :foreign_key => "pays"
-  has_many :joueur
+  has_many :joueur, :foreign_key => "club"
   has_many :match #, :class_name => "Match"
 end
