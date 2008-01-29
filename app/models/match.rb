a8dd5dd4d0e5dc9a6b6bae57ca03af817732c8e8
rdewolff <rdewolff@90076dac-8a41-0410-b28f-0f618caa8d27>
2008-01-29 19:02:32 +0000

    version avec liens dynamique OK

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,7 +1,7 @@
 class Match < ActiveRecord::Base
   belongs_to :groupe
   belongs_to :arbitre
-  belongs_to :equipe, :foreign_key => "equipeA"
-  belongs_to :equipe, :foreign_key => "equipeB"
+  belongs_to :equipea, :class_name => "Equipe", :foreign_key => "equipeA"
+  belongs_to :equipeb, :class_name => "Equipe", :foreign_key => "equipeB"
   has_and_belongs_to_many :parieur
 end
