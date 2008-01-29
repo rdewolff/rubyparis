3bd846677d09fae489e3acc3fa6b0dfbbca664d8
dasilva.bruno <dasilva.bruno@90076dac-8a41-0410-b28f-0f618caa8d27>
2008-01-29 11:24:46 +0000

    alors voilà

---
+++ db/migrate/016_create_competitions_parieurs.rb
@@ -0,0 +1,16 @@
+class CreateCompetitionsParieurs < ActiveRecord::Migration
+  def self.up
+    create_table ("competitions_parieurs", id => false) do |t|
+      t.column "parieur_id", :int, :null => false
+      t.column "competition_id", :int, :null => false
+      t.column "resultat", :int, :null => false
+    end
+    execute "alter table competitions_parieurs add foreign key (parieur_id) references parieurs(id)"
+    execute "alter table competitions_parieurs add foreign key (competition_id) references competitions(id)"
+    execute "alter table competitions_parieurs add foreign key (resultat) references equipes(id)"
+  end
+
+  def self.down
+    drop_table :competitions_parieurs
+  end
+end
