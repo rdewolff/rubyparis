91be55b0557652b1c16fe70ebfc1f41b5841a4de
dasilva.bruno <dasilva.bruno@90076dac-8a41-0410-b28f-0f618caa8d27>
2008-01-29 11:24:46 +0000

    alors voilà

---
+++ trunk/db/migrate/015_create_matchs_parieurs.rb
@@ -0,0 +1,15 @@
+class CreateMatchsParieurs < ActiveRecord::Migration
+  def self.up
+    create_table("matchs_parieurs", id => false) do |t|
+      t.column "parieur_id", :int, :null => false
+      t.column "match_id", :int, :null => false
+      t.column "resultat", :int, :null => false
+    end
+    execute "alter table matchs_parieurs add foreign key (parieur_id) references parieurs(id)"
+    execute "alter table matchs_parieurs add foreign key (match_id) references matchs(id)"
+  end
+
+  def self.down
+    drop_table :matchs_parieurs
+  end
+end
