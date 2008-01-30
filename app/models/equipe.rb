43a78c62607c5213daf3a9a488d0377ba9431dce
rdewolff <rdewolff@90076dac-8a41-0410-b28f-0f618caa8d27>
2008-01-30 12:20:06 +0000

    competitions

--- app/models/equipe.rb
+++ app/models/equipe.rb
@@ -2,5 +2,5 @@ class Equipe < ActiveRecord::Base
   belongs_to :entraineur
   belongs_to :nation, :foreign_key => "pays"
   has_many :joueur, :foreign_key => "club"
-  has_many :match #, :class_name => "Match"
+  has_many :match
 end
